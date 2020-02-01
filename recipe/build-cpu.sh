1c971bdc81b122311cd3fc76fff3eb68163f12fe
Henry Schreiner <henryschreineriii@gmail.com>
2020-01-31 23:22:11 -0500

    Proper arch flags

--- recipe/build-cpu.sh
+++ recipe/build-cpu.sh
@@ -3,6 +3,7 @@ set -evx
 
 export MAKEFLAGS="-j${CPU_COUNT}"
 export GOOFIT_DEVICE=OMP
+export GOOFIT_OPTI="-march=nocona;-mtune=haswell;-ftree-vectorize"
 
 rm pyproject.toml || echo "Already removed pyproject file"
-$PYTHON -m pip install --no-deps --ignore-installed -v .
+VERBOSE=1 $PYTHON -m pip install --no-deps --ignore-installed -v .
--- recipe/build-gpu.sh
+++ recipe/build-gpu.sh
@@ -4,6 +4,7 @@ set -evx
 export MAKEFLAGS="-j${CPU_COUNT}"
 export GOOFIT_DEVICE=CUDA
 export GOOFIT_ARCH="3.0;5.0;6.0;7.0+PTX"
+export GOOFIT_OPTI="-march=nocona;-mtune=haswell;-ftree-vectorize"
 
 rm pyproject.toml || echo "Already removed pyproject file"
 VERBOSE=1 $PYTHON -m pip install --no-deps --ignore-installed -v .
